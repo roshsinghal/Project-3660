3744f99c034c91cf531b499a7d26411fd450bf07
Dude00 <zacdudeguy@gmail.com>
2016-03-20 16:45:45 -0600

    testing here

--- login.php
+++ login.php
@@ -9,6 +9,7 @@ else
 // Define $username and $password
 $username=$_POST['username'];
 $password=$_POST['password'];
+echo "$_POST['username'] $_POST['password']";
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
 $connection = mysql_connect('17carson.cs.uleth.ca', 'group7', 'zpakwn');
 // To protect MySQL injection for Security purpose

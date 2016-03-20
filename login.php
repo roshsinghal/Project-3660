863cc70f35ae9ecbf442d8854065d784ee2089f6
Dude00 <zacdudeguy@gmail.com>
2016-03-20 16:52:40 -0600

    login changes woo

--- login.php
+++ login.php
@@ -7,9 +7,9 @@ $error = "Username $_POST[username] or Password $_POST[password] is invalid";
 else
 {
 // Define $username and $password
-$username="$_POST['username']";
-$password="$_POST['password']";
-echo "$_POST['username'] $_POST['password']";
+$username="$_POST[username]";
+$password="$_POST[password]";
+echo "$_POST[username] $_POST[password]";
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
 $connection = mysql_connect('17carson.cs.uleth.ca', 'group7', 'zpakwn');
 // To protect MySQL injection for Security purpose
@@ -20,7 +20,7 @@ $password = mysql_real_escape_string($password);
 // Selecting Database
 $db = mysql_select_db('group7', $connection);
 // SQL query to fetch information of registerd users and finds user match.
-$query = mysql_query("select * from login where password='$password' AND username='$username'", $connection);
+$query = mysql_query("select * from users where password='$password' AND username='$username'", $connection);
 $rows = mysql_num_rows($query);
 if ($rows == 1) {
 $_SESSION['login_user']=$username; // Initializing Session

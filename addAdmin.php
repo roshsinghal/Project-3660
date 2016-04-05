20a604c6e1c0fed274567e82182bacf3e7c6129b
Dude00 <zacdudeguy@gmail.com>
2016-04-05 16:42:58 -0600

    admin pages

---
+++ addAdmin.php
@@ -0,0 +1,94 @@
+<?php
+session_start(); // Starting Session
+if($_SESSION['regErr'])
+{
+	$regErr = $_SESSION['regErr'];
+	//destroy this immediately before bad things happen
+	unset($_SESSION['regErr']);
+}
+
+if(!$_SESSION['is_admin'])
+{
+	header('location: index.php');
+}
+?>
+<html>
+<head>
+<link href="mainCSS.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="jquery.leanModal.min.js"></script>
+</head>
+
+<title>
+Register|Book Rental Service
+</title>
+<body>
+
+<?php
+ require 'navigation.php';
+?>
+
+<h1 align="center">Add Administrator Account</h1>
+
+<form action="addAdmin2.php" method="post">
+<br>
+<?php
+echo 'Username: <input type="text" name="userName"';
+if($regErr)
+{
+	if(!$regErr['userName'])
+		echo '><font color=red>* Cannot be empty!</font';
+	else
+		echo " value=\"$regErr[userName]\"";
+}
+echo '><br>';
+echo 'Password: <input type="text" name="password"';
+if($regErr)
+{
+	if(!$regErr['password'])
+		echo '><font color=red>* Cannot be empty!</font';
+	else
+		echo " value=\"$regErr[password]\"";
+}
+echo '><br>';
+echo 'Full Name: <input type="text" name="fullName"';
+if($regErr)
+{
+	if(!$regErr['fullName'])
+		echo '><font color=red>* Cannot be empty!</font';
+	else
+		echo " value=\"$regErr[fullName]\"";
+}
+echo '><br>';
+echo 'Email: <input type="text" name="email"';
+if($regErr)
+{
+	if(!$regErr['email'])
+		echo '><font color=red>* Cannot be empty!</font';
+	else
+		echo " value=\"$regErr[email]\"";
+}
+echo '><br>';
+echo 'Address: <input type="text" name="address"';
+if($regErr)
+{
+	if(!$regErr['address'])
+		echo '><font color=red>* Cannot be empty!</font';
+	else
+		echo " value=\"$regErr[address]\"";
+}
+echo '><br>';
+echo 'Phone Number: <input type="text" name="phoneNumber"';
+if($regErr)
+{
+	if(!$regErr['phoneNumber'])
+		echo '><font color=red>* Cannot be empty!</font';
+	else
+		echo " value=\"$regErr[phoneNumber]\"";
+}
+echo '><br>';
+?>
+<input type="submit" value="Submit">
+</form>
+</body>
+</html>
---
+++ addAdmin2.php
@@ -0,0 +1,100 @@
+<?php
+session_start(); // Starting Session
+
+	$username = 'group7';
+	$password = 'zpakwn';
+	$_SESSION['regErr'] = array();
+	
+	if(empty($_POST['userName']))
+	{
+		$_SESSION['regErr']['userName'] = False;
+		$kill = True;	
+	}
+	else
+	{
+		$_SESSION['regErr']['userName'] = $_POST['userName'];
+	}
+	
+	if(empty($_POST['password']))
+	{
+		$_SESSION['regErr']['password'] = False;
+		$kill = True;
+	}
+	else
+	{
+		$_SESSION['regErr']['password'] = $_POST['password'];
+	}
+	
+	if(empty($_POST['address']))
+	{
+		$_SESSION['regErr']['address'] = False;
+		$kill = True;
+	}
+	else
+	{
+		$_SESSION['regErr']['address'] = $_POST['address'];
+	}
+	
+	if(empty($_POST['phoneNumber']))
+	{
+		$_SESSION['regErr']['phoneNumber'] = False;
+		$kill = True;	
+	}
+	else
+	{
+		$_SESSION['regErr']['phoneNumber'] = $_POST['phoneNumber'];
+	}
+	
+	if(empty($_POST['email']))
+	{
+		$_SESSION['regErr']['email'] = False;
+		$kill = True;	
+	}
+	else
+	{
+		$_SESSION['regErr']['email'] = $_POST['email'];
+	}
+	
+	if(empty($_POST['fullName']))
+	{
+		$_SESSION['regErr']['fullName'] = False;
+		$kill = True;
+	}
+	else
+	{
+		$_SESSION['regErr']['fullName'] = $_POST['fullName'];
+	}
+	
+	
+	if($kill)
+	{
+		header('location: register.php');
+		die();
+	}
+	else
+	{
+		unset($_SESSION['regErr']);
+	}
+
+try{
+	$conn = new PDO('mysql:host=17carson.cs.uleth.ca;dbname=group7',$username,$password);
+} catch (Exception $e) {
+    die("Unable to connect: " . $e->getMessage());
+}
+try {
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	$conn->beginTransaction();;
+	$conn->exec("insert into users (user_name, password, address, phone_number, e_mail, full_name, is_admin) values ('$_POST[userName]','$_POST[password]','$_POST[address]','$_POST[phoneNumber]','$_POST[email]', '$_POST[fullName]', 1)");
+	$conn->commit();
+	
+	echo "<h3>Admin ";
+	echo ($_POST["userName"]);
+	echo " added!</h3>";
+	header('location: index.php');
+
+} catch (Exception $e) {
+  $conn->rollBack();
+  echo "Failed: " . $e->getMessage();
+}
+?>

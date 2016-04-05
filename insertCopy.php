c7e8c5d130ffe90682686e92135ecc028390c54a
Dude00 <zacdudeguy@gmail.com>
2016-04-05 15:41:31 -0600

    inserting copies makes more sense if you're inserting it as a copy of a book and not a book of a copy
    i hope nobody's actually reading these

--- insertCopy.php
+++ insertCopy.php
@@ -11,7 +11,7 @@ try {
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	$conn->beginTransaction();;
-	$conn->exec("insert into book_availability (copy_id,book_id,availability_status) values ('$_POST[bookID]','$_POST[copyID]',1)");
+	$conn->exec("insert into book_availability (copy_id,book_id,availability_status) values ('$_POST[copyID]','$_POST[bookID]',1)");
 	$conn->exec("update book_details set no_of_copies=no_of_copies+1, number_available=number_available+1 where book_id='$_POST[bookID]'");
 	$conn->commit();
 	//echo "insert into inactive_copies (book_id, copy_id, bcondition) values ('$_POST[bookID]','$_POST[copyID]','$_POST[reason]')<br>";

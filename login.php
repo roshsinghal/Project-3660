c6bd8948776ce5f83e0b03e9ad32283d422c55e5
Dude00 <zacdudeguy@gmail.com>
2016-04-08 16:08:03 -0600

    debugging yay...

--- login.php
+++ login.php
@@ -43,7 +43,7 @@ else
 		}
 			//header("location: index.php"); // Redirecting To Other Page
 	} else {
-		$error = "Username or Password is invalid";
+		$error = "Username $username or Password $password is invalid";
 	}
 	mysql_close($connection); // Closing Connection
 }

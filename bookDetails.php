ba983fecae8018a299cd7f51722679127bed2c6a
roshsinghal <roshsinghal@hotmail.com>
2016-04-09 13:21:28 -0600

    login windowm in bookdetails

--- bookDetails.php
+++ bookDetails.php
@@ -59,8 +59,11 @@ Book Rental Service
 			}
 			else
 			{
+
+			
+
 				//ugly and non-functional, fix this
-				//echo '<ul><li><a id="modal_trigger" href="#modal" class="btn_log">Login</a></li></ul>';
+				echo '<ul><li><a id="trigger_mod" href="#fire_mod" class="btn_log">ORDER</a></li></ul>';
 			}
 		}
 	}
@@ -69,6 +72,77 @@ Book Rental Service
 		echo '<p>Book does not exist.</p>'; 
 	}
 ?>
+
+	<div id="fire_mod" class="popupContainer" style="display:none;">
+		<header class="popupHeader">
+			<span class="header_title">Login</span>
+			<span class="modal_close"><i class="fa fa-times"></i></span>
+		</header>
+		
+		<section class="popupBody">
+			<!-- Select login Login -->
+			<div class="select_login">
+				
+				<div class="action_btns">
+					<div class="one_half"><a href="#" id="form_login" class="btn">Login</a></div>
+					<div class="one_half last"><a href="register.php" id="register_form" class="btn">Register</a></div>
+				</div>
+			</div>
+
+			<!-- Username & Password Login form -->
+			<div class="user_login">
+				<form action="login.php" method="post">
+					<label>Username</label>
+					<input type="text" name="username" />
+					<br />
+
+					<label>Password</label>
+					<input type="password" name="password" />
+					<br />
+					
+					<?php 
+						echo "<input type=\"hidden\" name=\"lastPage\" value=\"$_SERVER[REQUEST_URI]\">";
+					?>
+
+					<div class="action_btns">
+						<div class="one_half"><a href="#" class="btn back_btn"><i class="fa fa-angle-double-left"></i> Back</a></div>
+						<div class="one_half last"><button type="submit" class="btn btn_red">Login</button></div>
+					</div>
+				</form>
+
+			</div>
+		</section>
+	</div>
+</div>
+
+<script type="text/javascript">
+	$("#trigger_mod").leanModal({top : 200, overlay : 0.6, closeButton: ".modal_close" });
+
+	$(function(){
+		// Calling Login Form
+		$("#form_login").click(function(){
+			$(".select_login").hide();
+			$(".user_login").show();
+			return false;
+		});
+
+		// Calling Register Form
+		$("#register_form").click(function(){
+			$(".select_login").close();
+			return false;
+		});
+
+		// Going back to Select Form
+		$(".back_btn").click(function(){
+			$(".user_login").hide();
+			$(".user_register").hide();
+			$(".select_login").show();
+			$(".header_title").text('Log In');
+			return false;
+		});
+
+	})
+</script>
 
 </body>
 </html>
